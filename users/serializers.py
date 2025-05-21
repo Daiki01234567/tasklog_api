2c561ec1fce4de2f05983cf77dc897490371f898
Daiki01234567 <gekkoaaa@gmail.com>
2025-05-21 13:34:29 +0900

    feat(users): make role field read-only in UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,9 @@
 from rest_framework import serializers
-from .models import User
 from core.serializers.user import BaseUserSerializer
 
 class UserSerializer(BaseUserSerializer):
+    role = serializers.CharField(read_only=True)
     password = serializers.CharField(write_only=True)
     
     class Meta(BaseUserSerializer.Meta):
         fields = BaseUserSerializer.Meta.fields + ['password']
-
-    def create(self, validated_data):
-        validated_data.pop('role', None)
-        password = validated_data.pop('password')
-        user = User.objects.create_user(password=password, **validated_data)
-        return user
